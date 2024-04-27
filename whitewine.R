391ffe468de991d6f9b570145827de98b6df900a
Ravisara Hasaranga <ravisarahdodampegamage@gmail.com>
2024-04-27 18:14:19 +0530

    Performed PCA, calculated eigenvalues/eigenvectors, and visualized cumulative score

--- whitewine.R
+++ whitewine.R
@@ -44,6 +44,11 @@ if (!require("tidyverse")) {
 
 
 library(readxl)
+library(factoextra)
+library(NbClust)
+library(cluster)
+library(cluster)
+library(stats)
 
 ##reading the dataset 
 whitewine<-read_excel(path="datasets/Whitewine_v6.xlsx")
@@ -73,3 +78,74 @@ boxplot(wine_cleaned,outline=FALSE)
 #scaling the dataset
 wine_subset <- as.data.frame(scale(wine_cleaned[,1:11]))
 boxplot(wine_subset,outline=FALSE)  
+
+
+#determining the clusters
+fviz_nbclust(wine_subset, kmeans, method="wss")
+fviz_nbclust(wine_subset,kmeans,method="silhouette")
+#fviz_nbclust(wine_subset,kmeans,method="gap_stat")
+
+#kmeans clustering
+kmeans_wine=kmeans(wine_subset,centers = 2,nstart = 10)
+kmeans_wine
+fviz_cluster(kmeans_wine,data=wine_subset)
+
+wine_centers<-kmeans_wine$centers
+cat("\n Cluster Centers: \n")
+print(wine_centers)
+
+# Within-Cluster Sum of Squares and Total Sum of Squares
+wss <- kmeans_wine$tot.withinss
+cat("\n Within cluster sums of squares : \n")
+wss
+
+tss <- kmeans_wine$totss
+cat("\n Total sum of squares: \n")
+tss
+
+# Between-Cluster Sum of Squares (BSS)
+bss <-kmeans_wine$betweenss
+cat("\n Between-Cluster Sum of Squares: \n")
+bss
+
+# Ratio of BSS/TSS
+bss_over_tss <- bss / tss
+cat("\n Ratio between BSS/TSS: \n")
+bss_over_tss
+
+#silhouette analysis
+silhouette_wine<-silhouette(kmeans_wine$cluster,dist(wine_subset)) 
+#need to use $cluster because just kmeans give other info
+fviz_silhouette(silhouette_wine)
+
+
+
+#pca
+wine_pca<-prcomp(wine_subset,center = TRUE,scale = TRUE)
+summary(wine_pca)
+
+#eigenvalues/eigenvectors
+eigenvalues<-(wine_pca$sdev)^2 #standard deviations (square roots of eigenvalues)
+cat("\n eigenvalues: \n")
+print(eigenvalues)
+
+eignenvectors<-wine_pca$rotation
+cat("\n eigenvectors: \n")
+print(eignenvectors)
+
+# Calculate cumulative proportion
+cumulative_proportion <- cumsum(eigenvalues)/ sum(eigenvalues)
+cat("\n Cumulative proportion: \n")
+cumulative_proportion
+
+# Plot cumulative score
+plot(cumulative_proportion, type = "b", 
+     xlab = "Number of Principal Components",
+     ylab = "Explained Varience",
+     main = "Cumulative Score per Principal Components")
+abline(0.85,0,col="red",lty=2)
+
+
+
+
+
