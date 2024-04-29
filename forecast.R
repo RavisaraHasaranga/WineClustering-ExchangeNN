b923313991332cbbdb95d34a02f7ec6ddaabceba
Ravisara Hasaranga <ravisarahdodampegamage@gmail.com>
2024-04-29 23:04:50 +0530

    Add NN training function with various architectures, activation functions, and output types

--- forecast.R
+++ forecast.R
@@ -1,48 +1,108 @@
 library(readxl)
-library(neuralnet)
 library(dplyr)
 
 data<-read_excel(path = "datasets/ExchangeUSD (2).xlsx")
-data<-data.frame(data)
+data<-data.frame(data[,3])
+names(data)<-c("rate")
 summary(data)
 
 #turn exchange rates in to a vector
-exchange_rate<-as.vector(data$"USD/EUR")
+exchange_rate<-as.vector(data$rate)
 exchange_rate
 
-#normalize the dataset
-data_norm<-as.data.frame(scale(data[,3]))
-names(data_norm) <- c("rate")
-data_norm
+
 
 #creating lagged datasets
-lag_data<-list()
+lagged_data<-list()
 
-lag_data[[1]]<- data_norm %>%
+lag_data[[1]]<- data %>%
     mutate(t.1 = lag(rate,1))%>%
     na.omit()
   
-lag_data[[2]] <- data_norm %>%
+lag_data[[2]] <- data %>%
   mutate(t.1 = lag(rate, 1),
          t.2 = lag(rate, 2)) %>%
   na.omit() 
 
-lag_data[[3]] <- data_norm %>%
+lag_data[[3]] <- data %>%
   mutate(t.1 = lag(rate, 1),
          t.2 = lag(rate, 2),
          t.3 = lag(rate, 3)) %>%
   na.omit() 
 
-lag_data[[4]] <- data_norm %>%
+lag_data[[4]] <- data %>%
   mutate(t.1 = lag(rate, 1),
          t.2 = lag(rate, 2),
          t.3 = lag(rate, 3),
          t.4 = lag(rate, 4)) %>%
   na.omit()
 
-lag_data
+#normalize each lagged dataset
+lag_data_norm<-list()
 
-#splitting data for training and testing
+for (i in 1:length(lag_data)){
+  lag_data_norm[[i]]<-scale(lag_data[[i]])
+}
+
+
+# Split data into training and testing sets
+lag_train_data <- list()
+lag_test_data <- list()
+
+for (i in 1: length(lag_data_norm)){
+  lag_train_data[[i]]<-lag_data_norm[[i]][1:400,]
+  lag_test_data[[i]]<-lag_data_norm[[i]][401:nrow(lag_data[[i]]),]
+
+}
+
+
+library(neuralnet)
+
+
+#defining hidden layers
+architectures<-list(c(5),c(7),c(13,9))
+
+#activation functions
+activate<-c("logistic","tanh")
+
+#output laters
+output_layer<-c(TRUE,FALSE) #true for linear, false for non linear
+
+#function to train data
+
+train<-function(architecture,train_data,test_data,activation,output){
+  
+  #creating a formula for each lag dataset
+  formula <- as.formula(paste("rate ~", paste(colnames(train_data)[-1],
+                                              collapse = " + ")))
+  
+  #training the model
+  train<-neuralnet(formula,train_data,act.fct = activation, linear.output = output)
+  
+  
+  # Predict on test data
+  predictions <- compute(model, test_data[, -1])$net.result
+  
+  
+}
+
+#list to store neural networks
+NN<-list()
+
+# Loop through lags, architectures, activation functions, and output types
+results <- list()
+for (i in 1:length(lag_train_data)) { 
+  for (j in 1:length(architectures)) { 
+    for (k in 1:length(activate)) { 
+      for (l in 1:length(output_layer)) { 
+        NN <- train(architectures[[j]], lag_train_data[[i]], lag_test_data[[i]], 
+                                     activate[k], output_layers[l])
+        results[[paste("lag", i, "arch", j, "act", k, "output", l)]] <- NN
+      }
+    }
+  }
+}
+
 
 
 
